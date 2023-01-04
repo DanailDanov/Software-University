634f436074e1e85bcf6243bb31844bfd4988bcc2
DanailDanov <114019922+DanailDanov@users.noreply.github.com>
2023-01-04 20:21:44 +0200

    delete

--- Programming fundamentals JavaScript/Basic Syntax, Conditional Statements and Loops - Lab/multiplyNumberBy2.js
+++
@@ -1,6 +0,0 @@
-function multiplyNumberBy2 (number) {
-
-    console.log(number * 2);
-}
-
-multiplyNumberBy2(5);
--- Programming fundamentals JavaScript/Basic Syntax, Conditional Statements and Loops - Lab/numbersFromNto1.js
+++
@@ -1,9 +0,0 @@
-function numbersFromNto1(num) {
-
-    for (let i = num; i > 0; i--) {
-        console.log(i);
-    }
-
-}
-
-numbersFromNto1(5);
